9f62d4b7d774a6acc3e97559371c5db3c81e1e35
Bala Ji <balaji10687@gmail.com>
2026-08-17 16:00:41 +0530

    style: load scanner border consistency fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import "./border-fix.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
